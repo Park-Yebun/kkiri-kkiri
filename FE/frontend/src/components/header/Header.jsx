7aa02c2ce0af05fe50ffc96ed228bdd2f152e0a5
yugyeong <starinsky284@gmail.com>
2024-03-27 14:59:58 +0900

    feat: 프론트 태블릿 규격 테스트중

--- FE/frontend/src/components/header/Header.jsx
+++ FE/frontend/src/components/header/Header.jsx
@@ -7,67 +7,69 @@ import ProfilePic from '../../assets/header/profilepic.png';
 const Headers = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 3rem 4rem;
+  padding: 1.5rem 1.5rem;
 `
 
 const Logo = styled.img`
-  height: 11rem;
-  width: 11rem;
+  height: 6.6rem;
+  width: 6.6rem;
 `
 const Profile = styled.div`
   position: relative;
   display: flex;
   text-align: right;
   right: 1rem;
-  width: 35rem;
-  height: 7rem;
+  width: 20rem;
+  height: 4rem;
   flex-shrink: 0;
-  border-radius: 3.75rem;
-  border: 2px solid #FFF;
+  border-radius: 3rem;
+  border: 1px solid #FFF;
   background: rgba(224, 224, 224, 0.50);
-  box-shadow: 5px 5px 5px 5px rgba(0, 0, 0, 0.25);
+  box-shadow: 2.5px 2.5px 2.5px 2.5px rgba(0, 0, 0, 0.25);
 `
 
 const ProfileAnimal = styled.img`
-  width: 7rem;
-  height: 7rem;
+  width: 4rem;
+  height: 4rem;
   flex-shrink: 0;
 `;
 
 const NameInfo = styled.div`
   display: flex;
-  padding-top: 1.5rem;
+  padding-top: 1rem;
   justify-content: center;
   color: #FFF;
-  -webkit-text-stroke-width: 0.15rem;
+  -webkit-text-stroke-width: 1.2px;
   -webkit-text-stroke-color: #000;
-  font-size: 3.75rem;
-  font-weight: 1000;
+  font-size: 2.2rem;
+  font-weight: 500;
   flex-grow:1;
 `;
 const NickName = styled.div`
 `;
 const NameType = styled.div`
-  margin: 0 1rem;
+  margin: 0 0.8rem;
 `;
 const DropdownMenu = styled.div`
   position: absolute;
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 28rem;
-  top: 10rem;
-  left: 7rem;
-  border-radius: 3.75rem;
-  border: 2px solid #FFF;
+  width: 15rem;
+  top: 5rem;
+  right: 0rem;
+  border-radius: 1rem;
+  border: 1px solid #FFF;
   background: rgba(224, 224, 224, 0.50);
   justify-content: center;
   color: #FFF;
-  -webkit-text-stroke-width: 0.15rem;
+  -webkit-text-stroke-width: 1.2px;
   -webkit-text-stroke-color: #000;
-  font-size: 3rem;
-  font-weight: 1000;
-  box-shadow: 5px 5px 5px 5px rgba(0, 0, 0, 0.25);
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  box-shadow: 2.6px 2.6px 2.6px 2.6px rgba(0, 0, 0, 0.25);
   display: ${props => props.show ? 'flex' : 'none'};
   opacity: 0;
   ${props => props.show && css`
@@ -77,13 +79,13 @@ const DropdownMenu = styled.div`
 `;
 
 const MenuItem = styled(Link)`
-  margin-top: 1rem;
+  margin-top: 0.6rem;
   cursor: pointer;
   color: inherit;
   text-decoration: none;
   &:visited {
     color: inherit;
-    -webkit-text-stroke-width: 0.15rem;
+    -webkit-text-stroke-width: 0.1rem;
     -webkit-text-stroke-color: #000;
   }
 `;
