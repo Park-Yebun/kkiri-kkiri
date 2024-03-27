87cc2fbf0beb74121ed5ca72caf7bdfbd18e350f
yugyeong <starinsky284@gmail.com>
2024-03-27 11:14:32 +0900

    feat: 프론트 빌드 오류 수정

--- FE/frontend/src/pages/StoryBookPage.jsx
+++ FE/frontend/src/pages/StoryBookPage.jsx
@@ -1,1 +1,15 @@
 import Background from '../components/common/Background';
+import background from '../assets/book/backimg.png';
+
+
+const StoryBookPage = () => {
+  return (
+    <Background backgroundimage={background}>
+
+
+
+    </Background>
+  );
+};
+
+export default StoryBookPage;
