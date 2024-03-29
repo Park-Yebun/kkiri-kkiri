2b8799948eab5dfcc19c78ebd852e52720b7d48b
류지원 <awldnjs2@naver.com>
2024-03-29 09:20:26 +0900

    [Back] fix: Nginx 테스트 코드

--- BE/src/main/java/com/kkirikkiri/domain/book/controller/TestController.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/controller/TestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api")
 @RestController
 @Slf4j
 public class TestController {
