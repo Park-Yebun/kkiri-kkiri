2e6b16ba12d0c064799f2c8286c88346b3b6f8a9
dbtywi <dbtywi@naver.com>
2024-03-29 10:05:30 +0900

    [Back] fix: CORS 권한 설정

---
+++ BE/src/main/java/com/kkirikkiri/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.kkirikkiri.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableScheduling
+@EnableTransactionManagement
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+}
