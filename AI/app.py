c940d435410b28e01ee0dfd635561f9642708a11
류지원 <awldnjs2@naver.com>
2024-03-31 15:07:34 +0900

    [Back] feat: 이미지 삭제 코드추가 및 gitignore 추가

--- AI/app.py
+++ AI/app.py
@@ -8,6 +8,13 @@ import os
 import boto3
 from botocore.exceptions import ClientError
 
+if torch.cuda.is_available():
+    device = torch.device("cuda")  # GPU 장치 설정
+    print("GPU를 사용합니다conda.")
+else:
+    device = torch.device("cpu")   # CPU 장치 설정
+    print("CPU를 사용합니다.")
+
 # Load environment variables from .env file
 load_dotenv()
 SPRINGBOOT_ENDPOINT = os.getenv("SPRINGBOOT_ENDPOINT")
@@ -19,19 +26,22 @@ S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 s3_client = boto3.client('s3',
                          aws_access_key_id=AWS_ACCESS_KEY_ID,
                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-
 app = FastAPI()
 
-
-model_id = "dallinmackay/Van-Gogh-diffusion"
+model_id = "proximasanfinetuning/luna-diffusion"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
-@app.post("/generate_image")
-async def generate_image(image_request: dict = Body(...)):
+# prompt = "fairy tale style, vivid, a scary monster appeared, but the boy bravely grabbed a cookie and chased the monster away, detailed, 8k" 
+# negative_prompt = "nsfw, bad quality, bad anatomy, worst quality, low quality, low resolutions, extra fingers, blur, blurry, ugly, wrongs proportions, watermark, image artifacts, lowres, ugly, jpeg artifacts, deformed, noisy image"
+        
+
+@app.post("/api/generate")
+async def generate_images(image_request: dict = Body(...)):
+    
     try:
         prompt = image_request.get("prompt")
-        image = pipe(prompt).images[0]
+        image = pipe(prompt=prompt, height=512, width=512).images[0]
         image_filename = str(uuid.uuid4())[:5] + ".png"
         image.save(image_filename)
 
@@ -40,6 +50,8 @@ async def generate_image(image_request: dict = Body(...)):
         s3_client.upload_file(image_filename, S3_BUCKET_NAME, s3_key)
         s3_image_url = f"https://{S3_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com/{s3_key}"
         print(s3_image_url)
+
+        os.remove(f'{image_filename}')
 
         # ImageResponse dto 
         image_response = {
