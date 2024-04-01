0bfc0f6498e41bc971d01f47e13a54ddf7cc96cc
dbtywi <dbtywi@naver.com>
2024-04-01 16:16:10 +0900

    [Front] fix: 이야기 생성 페이지 프롬프트 수정

---
+++ FE/frontend/src/pages/UccTest.jsx
@@ -0,0 +1,11 @@
+import background from '../assets/main/kkirikkiri-splash.png';
+import Background from '../components/common/Background';
+
+const UccTestPage = () => {
+    return (
+      <Background backgroundimage={background}>
+      </Background>
+  );
+};
+
+export default UccTestPage;
